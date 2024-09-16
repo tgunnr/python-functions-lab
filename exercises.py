3eeae649dec5b0a2de3fe7d6e3d59aa9d82f9c11
tgunnr <finalresidentevilfantasy@gmail.com>
2024-09-16 02:57:07 -0700

    exercise 8

--- exercises.py
+++ exercises.py
@@ -123,3 +123,23 @@ def calculate_tip(bill, percentage):
     return tip
 
 print('Exercise 7:', calculate_tip(50, 20))
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+
+def product(*args):
+    result = 1
+    for number in args:
+        result *= number
+    return result
+
+print('Exercise 8:', product(2, 5, 5))
+print('Exercise 8:', product(-1, 4))
